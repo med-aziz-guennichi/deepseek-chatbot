3f7b436be339c11d01392b24edfe6542b5f513f6
medazizguennichi <124068781+med-aziz-guennichi@users.noreply.github.com>
2025-02-08 12:41:35 +0100

    initialisation the repo

---
+++ database/user.database.ts
@@ -0,0 +1,19 @@
+import { model, models, Schema, Types, Document } from "mongoose";
+
+export interface IUser extends Document {
+    _id: Types.ObjectId
+    firstName: string,
+    clerkId: string,
+    lastName: string,
+    email: string
+}
+
+const userSchema = new Schema<IUser, {}, {}>({
+    firstName: String,
+    clerkId: String,
+    lastName: String,
+    email: String
+}, { timestamps: true, });
+
+const User = models?.User || model<IUser>("User", userSchema);
+export default User;
